5190a049eae07eaf928b821562dd57e359054817
Sid Saythongphet <sid.saythongphet@gmail.com>
2023-03-14 18:07:04 -0400

    add skills component

---
+++ src/component/Skills.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const Skills = () => {
+  return (
+    <div id='skills' class='min-h-screen pt-28'>
+      <h1 class='text-center text-6xl mb-10'>Skills</h1>
+      <div class='flex justify-center'>
+        <div class='flex justify-center items-center h-28 w-44 bg-slate-50 m-2 rounded-2xl'>
+          <img class='h-20' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" />  
+        </div>
+        <div class='flex justify-center items-center h-28 w-44 bg-slate-50 m-2 rounded-2xl'>
+          <img class='h-20' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" />  
+        </div>
+        <div class='flex justify-center items-center h-28 w-44 bg-slate-50 m-2 rounded-2xl'>
+          <img class='h-20' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />  
+        </div>
+        <div class='flex justify-center items-center h-28 w-44 bg-slate-50 m-2 rounded-2xl'>
+          <img class='h-20' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original-wordmark.svg" />  
+        </div>
+        <div class='flex justify-center items-center h-28 w-44 bg-slate-50 m-2 rounded-2xl'>
+          <img class='h-20' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" />  
+        </div>
+        <div class='flex justify-center items-center h-28 w-44 bg-slate-50 m-2 rounded-2xl'>
+          <img class='h-20' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/rails/rails-plain-wordmark.svg" />  
+        </div>
+        <div class='flex justify-center items-center h-28 w-44 bg-slate-50 m-2 rounded-2xl'>
+          <img class='h-20' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ruby/ruby-original.svg" />  
+        </div>
+        <div class='flex justify-center items-center h-28 w-44 bg-slate-50 m-2 rounded-2xl'>
+          <img class='h-20' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg" />  
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Skills
