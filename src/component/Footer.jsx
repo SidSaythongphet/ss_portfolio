c15f15ba6b475820abdccf8c3a188d2003e92ddd
Sid Saythongphet <sid.saythongphet@gmail.com>
2023-03-13 20:13:33 -0400

    add footer

---
+++ src/component/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div class="fixed bottom-0">
+      <h1 class="">
+        Copyright © | Sid Saythongphet | Portfolio
+      </h1>
+    </div>
+  )
+}
+
+export default Footer
