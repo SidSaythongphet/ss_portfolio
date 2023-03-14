2545acf0a6e1a7873730b94b3566c4da2bcac2ea
Sid Saythongphet <sid.saythongphet@gmail.com>
2023-03-13 20:13:47 -0400

    add hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from "react"
 import AboutMe from "./component/AboutMe"
+import Footer from "./component/Footer"
+import Hero from "./component/Hero"
+import NavBar from "./component/NavBar"
 
 function App() {
   return (
-    <>
+    <div class="flex flex-col">
+      <NavBar />
+      <Hero />
       <AboutMe />
-    </>
+      <Footer />
+    </div>
   )
 }
 
---
+++ src/component/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='flex flex-col h-screen w-full bg-slate-700 justify-center'>
+      <h1 className='text-center'>Hello, I am</h1>
+      <h1 className='text-center'>Sid Saythongphet</h1>
+      <h1 className='text-center'>Software Developer</h1>
+    </div>
+  )
+}
+
+export default Hero
