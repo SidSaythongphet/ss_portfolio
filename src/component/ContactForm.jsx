d31d1610084a01618f6eeec69950e29d9f280d59
Sid Saythongphet <sid.saythongphet@gmail.com>
2023-03-19 16:28:03 -0400

    connect emailjs to app

--- src/component/ContactForm.jsx
+++ src/component/ContactForm.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { send } from 'emailjs-com';
 
 const ContactForm = () => {
   const [formData, setFormData] = useState({
@@ -7,7 +8,24 @@ const ContactForm = () => {
     message: ''
   })
 
-  function handleSubmit(e) {
+  async function handleSubmit(e) {
+    e.preventDefault()
+    try {
+      await send(
+        import.meta.env.VITE_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+        formData,
+        import.meta.env.VITE_EMAILJS_USER_ID
+      )
+      alert('You made contact!')
+      setFormData({
+        fullName: '',
+        email: '',
+        message: ''
+      })
+    } catch (error) {
+      alert("Sorry, message did not send!")
+    }
 
   }
 
@@ -24,6 +42,7 @@ const ContactForm = () => {
         <input type='text' name='fullName' placeholder='Your Name' value={ formData.fullName } onChange={ handleChange } class='w-full text-black p-1'/>
         <input type='email' name='email' placeholder='Email' value={ formData.email } onChange={ handleChange } class='w-full mt-3 text-black p-1'/>
         <input type='text' name='message' placeholder='Message' value={ formData.message } onChange={ handleChange } class='w-full mt-3 h-40 text-black p-1'/>
+        <button onClick={ handleSubmit }>Submit</button>
       </form>
     </div>
   )
