54443c95ca3c08561857f90d2ba7d9794c6cd5af
Sid Saythongphet <sid.saythongphet@gmail.com>
2023-03-21 22:32:43 -0400

    position external link buttons

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import ContactForm from './ContactForm'
 
 
 const Contact = () => {
   return (
-    <div id='contact' class='min-h-screen  pt-36'>
+    <div id='contact' class='relative min-h-screen  pt-36'>
       <h1 class='text-center text-6xl mb-10'>Contact</h1>
       <ContactForm />
-      <div class='flex justify-center'>
+      <div class='absolute bottom-0 flex justify-center'>
         <div class='flex justify-center items-center h-28 w-44 bg-slate-50 m-2 rounded-2xl hover:mt-1'>
           <a href='https://www.linkedin.com/in/sid-saythongphet/' target='_blank' rel="noopener noreferrer">
             <img class='h-16' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg" />  
