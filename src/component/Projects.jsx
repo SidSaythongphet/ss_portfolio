98c62bf558f336f8c7e16c704e681730de83d735
Sid Saythongphet <sid.saythongphet@gmail.com>
2023-03-25 22:15:09 -0400

    style projects card template

--- src/component/Projects.jsx
+++ src/component/Projects.jsx
@@ -6,21 +6,27 @@ const Projects = () => {
       <h1 class='text-center text-6xl mb-10 w-screen'>Projects</h1>
       <div class='grid grid-cols-3 h-96 px-32'>
         <div class='flex justify-center px-10'>
-          <div class='h-full w-full bg-green-500'>
-            <h1 class='text-center p-5'>Project Name</h1>
-            <img class='h-10' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" />  
+          <div class='relative h-full w-full bg-green-500 rounded-2xl'>
+            <h1 class='flex absolute inset-x-0 top-5 justify-center text-xl'>Project Name</h1>
+            <div class='flex absolute inset-x-0 bottom-5 justify-center'>
+              <img class='h-10' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" />  
+            </div>
           </div>
         </div>
         <div class='flex justify-center px-10'>
-          <div class='h-full w-full bg-green-500'>
-            <h1 class='text-center p-5'>Project Name</h1>
-            <img class='h-10' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" />  
+          <div class='relative h-full w-full bg-green-500 rounded-2xl'>
+            <h1 class='flex absolute inset-x-0 top-5 justify-center text-xl'>Project Name</h1>
+            <div class='flex absolute inset-x-0 bottom-5 justify-center'>
+              <img class='h-10' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" />  
+            </div>
           </div>
         </div>
         <div class='flex justify-center px-10'>
-          <div class='h-full w-full bg-green-500'>
-            <h1 class='text-center p-5'>Project Name</h1>
-            <img class='h-10' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" />  
+          <div class='relative h-full w-full bg-green-500 rounded-2xl'>
+            <h1 class='flex absolute inset-x-0 top-5 justify-center text-xl'>Project Name</h1>
+            <div class='flex absolute inset-x-0 bottom-5 justify-center'>
+              <img class='h-10' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" />  
+            </div>
           </div>
         </div>
       </div>
