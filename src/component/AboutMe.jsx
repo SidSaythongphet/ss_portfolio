77420fb7f3e7b7990b40b02825d7cf8126312c2a
Sid Saythongphet <sid.saythongphet@gmail.com>
2023-03-25 22:29:23 -0400

    add break point for hero component

--- src/component/AboutMe.jsx
+++ src/component/AboutMe.jsx
@@ -4,12 +4,12 @@ const AboutMe = () => {
   return (
     <div id='aboutMe' class='flex flex-col min-h-screen pt-36'>
       <h1 class='text-center text-6xl mb-10 w-screen'>About Me</h1>
-      <div class='grid grid-cols-3'>
+      <div class='grid sm:grid-cols-3'>
         <div class='col-span-2 text-center'>
           <h1 class='text-xl'>Hello</h1>
           <h1>My name is Sid</h1>
         </div>
-        <div class="relative w-96 h-96">
+        <div class="w-96 h-96">
           <img class="rounded-full shadow-sm" src="src/assets/IMG_4551.JPG" alt="user image" />
         </div>
       </div>
--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div id='hero' class='flex flex-col h-screen w-full bg-[url(src/assets/rm373batch2-08.jpg)] bg-cover justify-start  pt-28'>
-      <h1 class='text-center text-5xl pt-28'>Hello, I am</h1>
-      <h1 class='text-center text-6xl pt-20'>Sid Saythongphet</h1>
-      <h1 class='text-center text-5xl pt-20'>Software Developer</h1>
+    <div id='hero' class='flex flex-col h-screen bg-[url(src/assets/rm373batch2-08.jpg)] bg-cover justify-start pt-28'>
+      <h1 class='text-center text-4xl sm:text-5xl pt-24 sm:pt-28'>Hello, I am</h1>
+      <h1 class='text-center text-5xl sm:text-6xl pt-20'>Sid Saythongphet</h1>
+      <h1 class='text-center text-4xl sm:text-5xl pt-20'>Software Developer</h1>
     </div>
   )
 }
