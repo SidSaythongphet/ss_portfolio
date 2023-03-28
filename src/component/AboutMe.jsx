9b3922899ea39648db42eeb03962f0ac4294c395
Sid Saythongphet <sid.saythongphet@gmail.com>
2023-03-27 23:36:53 -0400

    add about me bio

--- src/component/AboutMe.jsx
+++ src/component/AboutMe.jsx
@@ -8,6 +8,7 @@ const AboutMe = () => {
         <div class='col-span-2 text-center'>
           <h1 class='text-xl'>Hello</h1>
           <h1>My name is Sid</h1>
+          <h1>Entry-level software engineer with experience working in full-stack development with proficiency in HTML, CSS, JavaScript, React, Ruby, Rails, and SQL. I possess years of creative, collaborative, and client-focused work via the health and wellness industry that I bring to an exciting new career in technology. I am passionate about learning new skills and taking pride in my work.</h1>
         </div>
         <div class="w-96 h-96">
           <img class="rounded-full shadow-sm" src="src/assets/IMG_4551.JPG" alt="user image" />
