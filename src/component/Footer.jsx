6c94dcef94434f6f12237e21b635bc8f0be87697
Sid Saythongphet <sid.saythongphet@gmail.com>
2023-05-11 22:19:35 -0400

    format and remove photo attribution

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
+    <div class='text-center text-xs text-text1 my-2'>
       <h1>Copyright © | Sid Saythongphet | Portfolio</h1>
-      <a href="https://www.freepik.com/free-vector/5g-networking-technology-background-with-blue-digital-line_16406299.htm#page=2&query=crypto%20background&position=7&from_view=keyword&track=ais">Image by rawpixel.com</a> on Freepik
+      {/* <a href="https://www.freepik.com/free-vector/5g-networking-technology-background-with-blue-digital-line_16406299.htm#page=2&query=crypto%20background&position=7&from_view=keyword&track=ais">Image by rawpixel.com</a> on Freepik */}
     </div>
   )
 }
