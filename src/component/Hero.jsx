457a18963aa48a22abfe98483ef91056a7176984
Sid Saythongphet <sid.saythongphet@gmail.com>
2023-05-11 23:06:18 -0400

    align hero component and add short bio

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import { Link } from 'react-scroll'
 
 const Hero = () => {
   return (
-    <div id='hero' class='flex flex-col h-screen bg-secondary justify-start pt-28'>
-      <h1 class='font-oxygen text-center text-4xl sm:text-4xl pt-24 sm:pt-28'>HELLO THERE! I'M <span class='font-caveat text-5xl sm:text-6xl pt-20'>Sid Saythongphet</span></h1>
-      <h1 class='font-oxygen text-center text-2xl sm:text-2xl pt-10'>A Software Developer based out of New York City.</h1>
-      <h1 class='font-oxygen text-center text-2xl sm:text-2xl pt-20'>Interested in working together?</h1>
-      <button class='bg-cyan-800 w-40 rounded-lg px-3 py-1'>Contact Me!</button>
+    <div id='hero' class='flex flex-col h-screen bg-secondary pt-28 px-12 md:px-48'>
+      <h1 class='font-oxygen text-2xl sm:text-3xl pt-24 sm:pt-28'>HELLO THERE! I'M </h1>
+      <h1 class='font-caveat text-5xl sm:text-6xl pt-5'>Sid Saythongphet</h1>
+      <h1 class='font-oxygen text-lg sm:text-xl pt-5 pr-12'>A Software Developer based out of New York City. With a passion to solve problems and flex my creative mind, I am here to bring your ideas to life.</h1>
+      <h1 class='font-oxygen text-lg sm:text-xl pt-20'>Interested in working together?</h1>
+      <button class='bg-accent w-40 rounded-lg font-bold px-3 py-1'>              
+        <Link activeClass="active" to="contact" spy={true} smooth={true} duration={500}>
+          CONTACT  
+        </Link> 
+      </button>
     </div>
   )
 }
