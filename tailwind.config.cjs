79b9becad7e480d334af074d7667d180863e6fdf
Sid Saythongphet <sid.saythongphet@gmail.com>
2023-05-11 22:16:07 -0400

    add theme color and font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,19 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'caveat': ['Caveat', 'cursive'],
+        'oxygen': ['Oxygen', 'sans-serif'],
+      },
+      colors: {
+        'primary': '#010d23',
+        'secondary': '#03223f',
+        'accent': '#09738a',
+        'text1': '#e19f41',
+        'text2': '#fccb6f',
+      },
+    },
   },
   plugins: [],
 }
