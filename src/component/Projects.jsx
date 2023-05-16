c645cea7e5502949c031ff9c455e4879e28f50fd
Sid Saythongphet <sid.saythongphet@gmail.com>
2023-05-15 23:27:12 -0400

    add react player and add project youtube video

--- src/component/Projects.jsx
+++ src/component/Projects.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import ReactPlayer from "react-player"
 
 const Projects = () => {
   return (
-    <div id='skills' class='flex flex-col min-h-screen pt-24 md:pt-36'>
+    <div id='projects' class='flex flex-col min-h-screen pt-24 md:pt-36'>
       <h1 class='text-center text-4xl md:text-6xl mb-10 w-screen'>Projects</h1>
       <div class='self-center w-4/5 border-t-2 border-text2 pb-5'></div>
       <div class='flex justify-center px-10 md:grid md:grid-cols-3'>
         <div class='relative h-full w-full md:col-start-2'>
-          <video src="src/assets/Plannr App - Made with Clipchamp.mp4" autoPlay loop controls/>
+          <ReactPlayer url="https://youtu.be/ZfZubb0R174" controls={false} loop={true} playing={true}/>
           <div class='flex absolute inset-x-0 bottom-[-20] justify-around'>
             <h1 class='text-xl font-bold'>PLANNR APP</h1>
             <a href='https://plannr-app.onrender.com/' target='_blank' rel="noopener noreferrer">
