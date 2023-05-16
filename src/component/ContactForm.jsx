673c61f5f13009e022bc83a830fab429bbf6fdc1
Sid Saythongphet <sid.saythongphet@gmail.com>
2023-05-16 09:31:55 -0400

    debug email

--- src/component/ContactForm.jsx
+++ src/component/ContactForm.jsx
@@ -11,11 +11,10 @@ const ContactForm = () => {
   async function handleSubmit(e) {
     e.preventDefault()
     try {
-      await send(
+      await emailjs.send(
         import.meta.env.VITE_EMAILJS_SERVICE_ID,
         import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
-        formData,
-        import.meta.env.VITE_EMAILJS_USER_ID
+        formData
       )
       alert('You made contact!')
       setFormData({
